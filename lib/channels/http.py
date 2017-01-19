77d58842ec328dc6d5161dd91a6f5838696f9ad9
Davide Tampellini <tampe125@gmail.com>
2017-01-19 14:45:53 +0100

    Improved imports The remote host is now a class property

--- lib/channels/http.py
+++ lib/channels/http.py
@@ -1,34 +1,39 @@
-import logging
 import requests
 from abstract import AbstractChannel
+from logging import getLogger
 
 
 class HttpChannel(AbstractChannel):
+    def __init__(self, client_id, queue_send, queue_recv, debug=False):
+        super(HttpChannel, self).__init__(client_id, queue_send, queue_recv, debug)
+
+        self._remote_host = 'http://localhost:8000/'
+
     def enabled(self):
         response = requests.get('https://www.google.com')
 
         return response.status_code == 200
 
     def connect(self):
-        logging.getLogger('aptpy').debug("[HTTP] Trying to contact the remote server")
+        getLogger('aptpy').debug("[HTTP] Trying to contact the remote server")
 
         try:
             cookies = {'XDEBUG_SESSION': 'PHPSTORM'} if self.debug else {}
 
-            response = requests.post('http://localhost:8000/',
+            response = requests.post(self._remote_host,
                                      json={'task': 'ping', 'client_id': self.client_id},
                                      cookies=cookies
                                      )
 
             if response.status_code != 200:
-                logging.getLogger('aptpy').debug("[HTTP] We can't connect to the remote server")
+                getLogger('aptpy').debug("[HTTP] We can't connect to the remote server")
                 self.connected = False
 
-            logging.getLogger('aptpy').debug("[HTTP] Successfully connected to the remote server")
+            getLogger('aptpy').debug("[HTTP] Successfully connected to the remote server")
             self.connected = True
 
         except requests.ConnectionError:
-            logging.getLogger('aptpy').debug("[HTTP] An error occurred while contacting the remote server")
+            getLogger('aptpy').debug("[HTTP] An error occurred while contacting the remote server")
             self.connected = False
 
     def _send(self):
@@ -38,18 +43,18 @@ class HttpChannel(AbstractChannel):
         try:
             cookies = {'XDEBUG_SESSION': 'PHPSTORM'} if self.debug else {}
 
-            response = requests.post('http://localhost:8000/',
+            response = requests.post(self._remote_host,
                                      json={'task': 'get_job', 'client_id': self.client_id},
                                      cookies=cookies
                                      )
 
             if response.status_code != 200:
-                logging.getLogger('aptpy').debug("[HTTP] We can't connect to the remote server")
+                getLogger('aptpy').debug("[HTTP] We can't connect to the remote server")
                 self.connected = False
                 return
 
             return response.json()
 
         except requests.ConnectionError:
-            logging.getLogger('aptpy').debug("[HTTP] An error occurred while contacting the remote server")
+            getLogger('aptpy').debug("[HTTP] An error occurred while contacting the remote server")
             self.connected = False
